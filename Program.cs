07fedd072c5d7d4ceb9ac99d1503c9e04ef8de1e
Sanjay Dwivedi <sanjaydwivedi04@gmail.com>
2017-11-20 11:18:40 +0530

    Update Program.cs
    Updated code

--- Program.cs
+++ Program.cs
@@ -8,16 +8,6 @@ namespace SolidPrincipile
 {
     class Program
     {
-        static void Palindrome()
-        {
-            string s, revs = string.Empty;
-            s = Console.ReadLine();
-            for (int i = s.Length - 1; i >= 0; i--)
-            {
-                revs += s[i].ToString();
-            }
-            Console.WriteLine(revs + "=" + s);
-        }
         static void Anagram()
         {
             Console.WriteLine("Enter Word 1");
